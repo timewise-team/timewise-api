671e5eb1319e9fed61a169fd685785ad602cf276
Builanviet <builanviet@gmail.com>
2024-10-14 18:08:09 +0700

    get workspace memberlist and invitation

--- feature/workspace_user/transport/register_workspace_user_handler.go
+++ feature/workspace_user/transport/register_workspace_user_handler.go
@@ -1,6 +1,7 @@
 package transport
 
 import (
+	"api/middleware"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -15,5 +16,9 @@ func RegisterWorkspaceUserHandler(router fiber.Router) {
 
 	// Register all endpoints here
 	router.Get("/get-workspace_user/email/:email?/workspace_id/:workspace_id?", workspaceUserHandler.getWorkspaceUserByEmailAndWorkspace)
+	//get workspace user list
+	router.Get("/workspace_user_list", middleware.CheckWorkspaceRole([]string{"owner", "admin", "member"}), workspaceUserHandler.getWorkspaceUserList)
+	//get workspace user invitation list
+	router.Get("/workspace_user_invitation_list", middleware.CheckWorkspaceRole([]string{"owner", "admin"}), workspaceUserHandler.getWorkspaceUserInvitationList)
 
 }
