f490a76889d81620e82433f85f740406c4f5867c
Builanviet <builanviet@gmail.com>
2024-11-09 18:13:32 +0700

    fix board column

--- feature/board_columns/transport/get_board_columns_by_workspace_handler.go
+++ feature/board_columns/transport/get_board_columns_by_workspace_handler.go
@@ -9,6 +9,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 	schedule_dtos "github.com/timewise-team/timewise-models/dtos/core_dtos"
 	dtos "github.com/timewise-team/timewise-models/dtos/core_dtos/board_columns_dtos"
+	"github.com/timewise-team/timewise-models/models"
 	"strconv"
 )
 
@@ -38,7 +39,12 @@ func (h *BoardColumnsHandler) getBoardColumnsByWorkspace(c *fiber.Ctx) error {
 			"message": "Invalid workspace ID",
 		})
 	}
-
+	workspaceUser := c.Locals("workspace_user").(*models.TwWorkspaceUser)
+	if workspaceUser.WorkspaceId != workspaceIDCheck {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"message": "Access denied",
+		})
+	}
 	// Get the board columns
 	boardColumns, err := board_columns.NewBoardColumnsService().GetBoardColumnsByWorkspace(workspaceID)
 	if err != nil {
@@ -69,60 +75,201 @@ func (h *BoardColumnsHandler) getBoardColumnsByWorkspace(c *fiber.Ctx) error {
 			})
 		}
 		var schedulesListResponse []schedule_dtos.TwScheduleListInBoardColumnResponse
-		for _, schedule := range schedules {
-			var schedulesList schedule_dtos.TwScheduleListInBoardColumnResponse
-			schedulesList.ID = schedule.ID
-			schedulesList.WorkspaceID = schedule.WorkspaceId
-			schedulesList.BoardColumnID = schedule.BoardColumnId
-			schedulesList.Title = schedule.Title
-			schedulesList.Description = schedule.Description
-			if schedule.StartTime != nil {
-				schedulesList.StartTime = *schedule.StartTime
-			}
-			if schedule.EndTime != nil {
-				schedulesList.EndTime = *schedule.EndTime
-			}
-			schedulesList.Location = schedule.Location
-			schedulesList.CreatedBy = schedule.CreatedBy
-			if schedule.CreatedAt != nil {
-				schedulesList.CreatedAt = *schedule.CreatedAt
-			}
-			if schedule.UpdatedAt != nil {
-				schedulesList.UpdatedAt = *schedule.UpdatedAt
-			}
-			schedulesList.Status = schedule.Status
-			schedulesList.AllDay = schedule.AllDay
-			schedulesList.Visibility = schedule.Visibility
-			schedulesList.VideoTranscript = schedule.VideoTranscript
-			schedulesList.ExtraData = schedule.ExtraData
-			schedulesList.IsDeleted = schedule.IsDeleted
-			schedulesList.RecurrencePattern = schedule.RecurrencePattern
-			schedulesList.Position = schedule.Position
-			schedulesList.Priority = schedule.Priority
-			scheduleParticipants, err := schedule_participant.NewScheduleParticipantService().GetScheduleParticipantsBySchedule(schedule.ID, workspaceID)
-			if err != nil {
-				return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-					"message": "The server failed to respond",
-				})
-			}
-			Documents, err := document.NewDocumentService().GetDocumentsBySchedule(schedule.ID)
-			if err != nil {
-				return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-					"message": "The server failed to respond",
-				})
-			}
-			Comments, err := comment.NewCommentService().GetCommentsBySchedule(schedule.ID)
-			if err != nil {
-				return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-					"message": "The server failed to respond",
-				})
-			}
-			schedulesList.ScheduleParticipants = scheduleParticipants
-			schedulesList.Documents = len(Documents)
-			schedulesList.Comments = len(Comments)
+		if workspaceUser.Role == "admin" || workspaceUser.Role == "owner" {
+			for _, schedule := range schedules {
+				var schedulesList schedule_dtos.TwScheduleListInBoardColumnResponse
+				schedulesList.ID = schedule.ID
+				schedulesList.WorkspaceID = schedule.WorkspaceId
+				schedulesList.BoardColumnID = schedule.BoardColumnId
+				schedulesList.Title = schedule.Title
+				schedulesList.Description = schedule.Description
+				if schedule.StartTime != nil {
+					schedulesList.StartTime = *schedule.StartTime
+				}
+				if schedule.EndTime != nil {
+					schedulesList.EndTime = *schedule.EndTime
+				}
+				schedulesList.Location = schedule.Location
+				schedulesList.CreatedBy = schedule.CreatedBy
+				if schedule.CreatedAt != nil {
+					schedulesList.CreatedAt = *schedule.CreatedAt
+				}
+				if schedule.UpdatedAt != nil {
+					schedulesList.UpdatedAt = *schedule.UpdatedAt
+				}
+				schedulesList.Status = schedule.Status
+				schedulesList.AllDay = schedule.AllDay
+				schedulesList.Visibility = schedule.Visibility
+				schedulesList.VideoTranscript = schedule.VideoTranscript
+				schedulesList.ExtraData = schedule.ExtraData
+				schedulesList.IsDeleted = schedule.IsDeleted
+				schedulesList.RecurrencePattern = schedule.RecurrencePattern
+				schedulesList.Position = schedule.Position
+				schedulesList.Priority = schedule.Priority
+				scheduleParticipants, err := schedule_participant.NewScheduleParticipantService().GetScheduleParticipantsBySchedule(schedule.ID, workspaceID)
+				if err != nil {
+					return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+						"message": "The server failed to respond",
+					})
+				}
+				Documents, err := document.NewDocumentService().GetDocumentsBySchedule(schedule.ID)
+				if err != nil {
+					return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+						"message": "The server failed to respond",
+					})
+				}
+				Comments, err := comment.NewCommentService().GetCommentsBySchedule(schedule.ID)
+				if err != nil {
+					return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+						"message": "The server failed to respond",
+					})
+				}
+				schedulesList.ScheduleParticipants = scheduleParticipants
+				schedulesList.Documents = len(Documents)
+				schedulesList.Comments = len(Comments)
 
-			schedulesListResponse = append(schedulesListResponse, schedulesList)
+				schedulesListResponse = append(schedulesListResponse, schedulesList)
 
+			}
+		} else if workspaceUser.Role == "member" {
+			for _, schedule := range schedules {
+				if schedule.Visibility == "public" || schedule.Visibility == "" {
+					var schedulesList schedule_dtos.TwScheduleListInBoardColumnResponse
+					schedulesList.ID = schedule.ID
+					schedulesList.WorkspaceID = schedule.WorkspaceId
+					schedulesList.BoardColumnID = schedule.BoardColumnId
+					schedulesList.Title = schedule.Title
+					schedulesList.Description = schedule.Description
+					if schedule.StartTime != nil {
+						schedulesList.StartTime = *schedule.StartTime
+					}
+					if schedule.EndTime != nil {
+						schedulesList.EndTime = *schedule.EndTime
+					}
+					schedulesList.Location = schedule.Location
+					schedulesList.CreatedBy = schedule.CreatedBy
+					if schedule.CreatedAt != nil {
+						schedulesList.CreatedAt = *schedule.CreatedAt
+					}
+					if schedule.UpdatedAt != nil {
+						schedulesList.UpdatedAt = *schedule.UpdatedAt
+					}
+					schedulesList.Status = schedule.Status
+					schedulesList.AllDay = schedule.AllDay
+					schedulesList.Visibility = schedule.Visibility
+					schedulesList.VideoTranscript = schedule.VideoTranscript
+					schedulesList.ExtraData = schedule.ExtraData
+					schedulesList.IsDeleted = schedule.IsDeleted
+					schedulesList.RecurrencePattern = schedule.RecurrencePattern
+					schedulesList.Position = schedule.Position
+					schedulesList.Priority = schedule.Priority
+					scheduleParticipants, err := schedule_participant.NewScheduleParticipantService().GetScheduleParticipantsBySchedule(schedule.ID, workspaceID)
+					if err != nil {
+						return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+							"message": "The server failed to respond",
+						})
+					}
+					Documents, err := document.NewDocumentService().GetDocumentsBySchedule(schedule.ID)
+					if err != nil {
+						return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+							"message": "The server failed to respond",
+						})
+					}
+					Comments, err := comment.NewCommentService().GetCommentsBySchedule(schedule.ID)
+					if err != nil {
+						return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+							"message": "The server failed to respond",
+						})
+					}
+					schedulesList.ScheduleParticipants = scheduleParticipants
+					schedulesList.Documents = len(Documents)
+					schedulesList.Comments = len(Comments)
+
+					schedulesListResponse = append(schedulesListResponse, schedulesList)
+				} else if schedule.Visibility == "private" {
+					//check member is participant or creator of schedule
+					scheduleParticipants, err := schedule_participant.NewScheduleParticipantService().GetScheduleParticipantsBySchedule(schedule.ID, workspaceID)
+					if err != nil {
+						return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+							"message": "The server failed to respond",
+						})
+					}
+					var isParticipant bool
+					for _, scheduleParticipant := range scheduleParticipants {
+						if scheduleParticipant.WorkspaceUserId == workspaceUser.ID && scheduleParticipant.InvitationStatus == "joined" {
+							isParticipant = true
+							break
+						}
+					}
+					if schedule.CreatedBy == workspaceUser.ID || isParticipant {
+						var schedulesList schedule_dtos.TwScheduleListInBoardColumnResponse
+						schedulesList.ID = schedule.ID
+						schedulesList.WorkspaceID = schedule.WorkspaceId
+						schedulesList.BoardColumnID = schedule.BoardColumnId
+						schedulesList.Title = schedule.Title
+						schedulesList.Description = schedule.Description
+						if schedule.StartTime != nil {
+							schedulesList.StartTime = *schedule.StartTime
+						}
+						if schedule.EndTime != nil {
+							schedulesList.EndTime = *schedule.EndTime
+						}
+						schedulesList.Location = schedule.Location
+						schedulesList.CreatedBy = schedule.CreatedBy
+						if schedule.CreatedAt != nil {
+							schedulesList.CreatedAt = *schedule.CreatedAt
+						}
+						if schedule.UpdatedAt != nil {
+							schedulesList.UpdatedAt = *schedule.UpdatedAt
+						}
+						schedulesList.Status = schedule.Status
+						schedulesList.AllDay = schedule.AllDay
+						schedulesList.Visibility = schedule.Visibility
+						schedulesList.VideoTranscript = schedule.VideoTranscript
+						schedulesList.ExtraData = schedule.ExtraData
+						schedulesList.IsDeleted = schedule.IsDeleted
+						schedulesList.RecurrencePattern = schedule.RecurrencePattern
+						schedulesList.Position = schedule.Position
+						schedulesList.Priority = schedule.Priority
+						scheduleParticipants, err := schedule_participant.NewScheduleParticipantService().GetScheduleParticipantsBySchedule(schedule.ID, workspaceID)
+						if err != nil {
+							return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+								"message": "The server failed to respond",
+							})
+						}
+						Documents, err := document.NewDocumentService().GetDocumentsBySchedule(schedule.ID)
+						if err != nil {
+							return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+								"message": "The server failed to respond",
+							})
+						}
+						Comments, err := comment.NewCommentService().GetCommentsBySchedule(schedule.ID)
+						if err != nil {
+							return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+								"message": "The server failed to respond",
+							})
+						}
+						schedulesList.ScheduleParticipants = scheduleParticipants
+						schedulesList.Documents = len(Documents)
+						schedulesList.Comments = len(Comments)
+
+						schedulesListResponse = append(schedulesListResponse, schedulesList)
+					} else {
+						var schedulesList schedule_dtos.TwScheduleListInBoardColumnResponse
+						schedulesList.ID = schedule.ID
+						schedulesList.WorkspaceID = schedule.WorkspaceId
+						schedulesList.BoardColumnID = schedule.BoardColumnId
+						schedulesList.Title = schedule.Title
+						schedulesList.Description = schedule.Description
+						schedulesList.Visibility = schedule.Visibility
+						schedulesList.Position = schedule.Position
+						schedulesList.Status = schedule.Status
+						schedulesList.ExtraData = "IsLocked"
+						schedulesListResponse = append(schedulesListResponse, schedulesList)
+					}
+
+				}
+			}
 		}
 		boardColumnsResponse.Schedules = schedulesListResponse
 		boardColumnsResponseList = append(boardColumnsResponseList, boardColumnsResponse)
