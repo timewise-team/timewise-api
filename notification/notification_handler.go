732c29c054353311670b1c4d34e1d85e4fa8cfd0
Khanh Hoang <khanhhn@metric.vn>
2024-11-19 23:51:02 +0700

    fix(notifications): remove validation

--- notification/notification_handler.go
+++ notification/notification_handler.go
@@ -2,16 +2,10 @@ package notification
 
 import (
 	"api/dms"
-	"errors"
 	"github.com/timewise-team/timewise-models/models"
 )
 
 func PushNotifications(notification models.TwNotifications) error {
-	// Validate required fields
-	if notification.UserEmailId == 0 || notification.Type == "" || notification.Message == "" {
-		return errors.New("Missing required fields")
-	}
-
 	// call dms to insert notification into database
 	_, err := dms.CallAPI("POST", "/notification", notification, nil, nil, 120)
 	if err != nil {
